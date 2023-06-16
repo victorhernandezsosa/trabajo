249b3b1e5be1ac6c40ea7c9ea1c1b141c67d3282
victorhernandezsosa <victorhernadez093@gmail.com>
2023-06-16 12:07:01 -0600

    Cambio realizaro el dia 16/06/2023

--- app/src/main/java/com/example/trabajo/ActivityFoto.java
+++ app/src/main/java/com/example/trabajo/ActivityFoto.java
@@ -5,23 +5,34 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class ActivityFoto extends AppCompatActivity {
 
     static final int peticion_captura_imagen = 101;
     static final int peticion_acceso_camara = 102;
 
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    String currentPhotoPath;
     ImageView Objetoimagen;
     Button btntomarfoto;
     String pathfoto;
@@ -51,7 +62,8 @@ public class ActivityFoto extends AppCompatActivity {
         }
         else
         {
-            TomarFoto();
+            dispatchTakePictureIntent();
+            //TomarFoto();
         }
     }
 
@@ -93,4 +105,44 @@ public class ActivityFoto extends AppCompatActivity {
         }
 
     }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.trabajo.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, peticion_captura_imagen);
+            }
+        }
+    }
+
 }
